db306cf3d2cc6c3c9e025dcb07f7bcd7a3e31c8a
Dennie Mok <mokdennie26@gmail.com>
2023-09-01 11:51:46 +1000

    Remove a lot of separated components

--- src/com/battleasya/bautility/BAUtility.java
+++ src/com/battleasya/bautility/BAUtility.java
@@ -1,23 +1,13 @@
 package com.battleasya.bautility;
 
 import com.battleasya.bautility.command.broadcast.*;
-import com.battleasya.bautility.command.staffchat.SC;
-import com.battleasya.bautility.command.staffchat.SCT;
-import com.battleasya.bautility.command.utility.Ping;
-import com.battleasya.bautility.command.utility.Remind;
 import com.battleasya.bautility.handler.PexBlock;
-import com.battleasya.bautility.handler.Reconnect;
-import com.battleasya.bautility.handler.StaffChat;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BAUtility extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-        /* staff chat */
-        getCommand("sc").setExecutor(new SC());
-        getCommand("sct").setExecutor(new SCT());
 
         /* staff broadcast */
         getCommand("abc").setExecutor(new ABC());
@@ -26,14 +16,8 @@ public class BAUtility extends JavaPlugin {
         getCommand("sbc").setExecutor(new SBC());
         getCommand("tbc").setExecutor(new TBC());
 
-        /* utility */
-        getCommand("ping").setExecutor(new Ping());
-        getCommand("remind").setExecutor(new Remind());
-
         /* listener */
         getServer().getPluginManager().registerEvents(new PexBlock(), this);
-        getServer().getPluginManager().registerEvents(new Reconnect(this), this);
-        getServer().getPluginManager().registerEvents(new StaffChat(), this);
 
     }
 
